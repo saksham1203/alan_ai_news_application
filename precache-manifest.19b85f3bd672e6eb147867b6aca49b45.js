d46d5edd61003dd9135d960d6207a7246ff8329d
saksham <saksham.s1203@gmail.com>
2020-10-02 13:45:23 +0530

    Updates

---
+++ precache-manifest.19b85f3bd672e6eb147867b6aca49b45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7863529ce155e1a428287fa8e3e144",
+    "url": "/alan_ai_news_application/index.html"
+  },
+  {
+    "revision": "bc2e0b5a55e51cfa6c57",
+    "url": "/alan_ai_news_application/static/css/main.8941d8a3.chunk.css"
+  },
+  {
+    "revision": "b0eaa24cf2927e5ad63c",
+    "url": "/alan_ai_news_application/static/js/2.e8f0a911.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/alan_ai_news_application/static/js/2.e8f0a911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2e0b5a55e51cfa6c57",
+    "url": "/alan_ai_news_application/static/js/main.79d19a12.chunk.js"
+  },
+  {
+    "revision": "ed6ab2eb1aa72b77e586",
+    "url": "/alan_ai_news_application/static/js/runtime-main.ae5fd319.js"
+  }
+]);
